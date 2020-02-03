63350bc0fee44270547c7585ab148962646a3b64
Kevin Lu <kevinlu1248@gmail.com>
2020-02-03 11:11:34 -0800

    added README and scripting

---
+++ README.md
@@ -0,0 +1,5 @@
+# Corona
+Python script to scrape stats regarding the Corona virus. You will need requests from pip3. Just run:
+```shell script
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 from re import *
+from time import time
 
 if __name__ == '__main__':
+    start = time()
+    print("Getting files...")
     r = requests.get("https://ncov.dxy.cn/ncovh5/view/pneumonia?scene=2&clicktime=1579582238&enterid=1579582238&from=singlemessage&isappinstalled=0")
-    # with open('page.html', 'w') as f: f.write(r.text)
+    print("Parsing...\n")
     regex = '"countRemark":"*","confirmedCount":(?P<confirmed>\d*),"suspectedCount":(?P<suspected>\d*),"curedCount":(?P<cured>\d*),"deadCount":(?P<dead>\d*),'
     s = search(regex, r.text)
     print('''Confirmed: {}\nSuspected: {}\nCured: {}\nDead: {}'''.format(s['confirmed'], s['suspected'], s['cured'], s['dead']))
-
+    print("\nWriting it to the file...")
+    with open('page.html', 'w') as f: f.write(r.text)
+    print("Done in {} seconds\n".format(time() - start))
